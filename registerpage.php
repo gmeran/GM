1546d5a21877ec6d1441f1287c2bc03d6e5af86b
gmeran <36177020+gmeran@users.noreply.github.com>
2024-03-08 00:30:20 -0500

    Added Modifcations to RegisterPager

--- registerpage.php
+++ registerpage.php
@@ -1,6 +1,22 @@
+<?php include 'include/header.php' ?>
 
-<?php include 'include/header.php'?>
 
-
-<h1>Welcome to GM HealthCare Registration  Page</h1>
+<h1>Welcome to GM HealthCare Registration Page</h1>
 <img src="img/gmlogo.png" class="w-25 mb-3" alt="">
+
+
+
+<div class="mb-3">
+    <label for="user" class="form-label">UserName</label>
+    <input type="text" class="form-control">
+</div>
+
+<div class="mb-3">
+    <label for="password" class="form-label">Password</label>
+    <input type="text " class="form-control">
+</div>
+
+<div class="mb-3">
+    <label for="email" class="form-label">Email</label>
+    <input type="email" class="form-control">
+</div>
